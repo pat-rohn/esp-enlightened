0e6e1bf44a4ed52dd9769ce1277519be43e90e33
Pat Rohn <schusterklein@live.com>
2024-02-03 12:19:21 +0100

    Toggle Shell with get request

--- src/events.h
+++ src/events.h
@@ -15,22 +15,15 @@
 
 void CallEvent()
 {
-    // TODO: Add to config
-    String msg = R"rawliteral(
-        {"id":"switch.set_16639312304270.5810851193709093",
-        "src":"localweb326",
-        "method":"switch.set",
-        "params":{"id":0,"on":false}}
-        )rawliteral";
-
     WiFiClient client = WiFiClient();
     HTTPClient http;
-    String serverPath = "http://192.168.1.120/rpc/Switch.Toggle?id=0";
+    //TODO: Add to config
+    String serverPath = "http://192.168.1.125/relay/0?turn=toggle";
     Serial.println(serverPath);
 
     http.begin(client, serverPath.c_str());
     Serial.println("Post data");
-    int httpResponseCode = http.POST(msg);
+    int httpResponseCode = http.GET();
 
     if (httpResponseCode > 0)
     {
